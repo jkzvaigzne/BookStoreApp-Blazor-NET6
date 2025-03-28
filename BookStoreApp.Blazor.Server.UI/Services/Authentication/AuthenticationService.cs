38823892bd006a389bcdc8b762dc6fbd04073de2
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2025-03-28 16:16:28 +0200

    Setup User Login First

---
+++ BookStoreApp.Blazor.Server.UI/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,36 @@
+﻿using Blazored.LocalStorage;
+using BookStoreApp.Blazor.Server.UI.Providers;
+using BookStoreApp.Blazor.Server.UI.Services.Base;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace BookStoreApp.Blazor.Server.UI.Services.Authentication
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly IClient httpClient;
+        private readonly ILocalStorageService localstorage;
+        private readonly AuthenticationStateProvider authenticationStateProvider;
+
+        public AuthenticationService(IClient httpClient,
+            ILocalStorageService localstorage,
+            AuthenticationStateProvider authenticationStateProvider)
+        {
+            this.httpClient = httpClient;
+            this.localstorage = localstorage;
+            this.authenticationStateProvider = authenticationStateProvider;
+        }
+
+        public async Task<bool> AuthenticateAsync(LoginUserDto loginModel)
+        {
+            var response = await httpClient.LoginAsync(loginModel);
+
+            // store token
+            await localstorage.SetItemAsync("accessToken", response.Token);
+
+            // change auth state of app
+            await ((ApiAuthenticationStateProvider)authenticationStateProvider).LoggedIn();
+
+            return true;
+        }
+    }
+}

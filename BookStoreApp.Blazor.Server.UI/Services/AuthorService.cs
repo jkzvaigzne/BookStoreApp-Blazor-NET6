b839b32264fa53f0b3782ee8b5db468414927d2f
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2025-03-29 16:43:48 +0200

    Extend HTTP Client

---
+++ BookStoreApp.Blazor.Server.UI/Services/AuthorService.cs
@@ -0,0 +1,36 @@
+﻿using Blazored.LocalStorage;
+using BookStoreApp.Blazor.Server.UI.Services.Base;
+
+namespace BookStoreApp.Blazor.Server.UI.Services
+{
+    public class AuthorService : BaseHttpService, IAuthorService
+    {
+        private readonly IClient client;
+        public AuthorService(ILocalStorageService localStorage, IClient client) : base(localStorage, client)
+        {
+            this.client = client;
+        }
+
+        public async Task<Response<List<AuthorReadOnlyDto>>> GetAuthors()
+        {
+            Response<List<AuthorReadOnlyDto>> response;
+
+            try
+            {
+                await GetBearerToken();
+                var data = await client.AuthorsAllAsync();
+                response = new Response<List<AuthorReadOnlyDto>>
+                {
+                    Data = data.ToList(),
+                    Success = true
+                };
+            } 
+            catch(ApiException exception)
+            {
+                response = ConvertApiExceptions<List<AuthorReadOnlyDto>>(exception);
+            }
+
+            return response;
+        }
+    }
+}
---
+++ BookStoreApp.Blazor.Server.UI/Services/Base/BaseHttpService.cs
@@ -0,0 +1,42 @@
+﻿using Blazored.LocalStorage;
+using System.Net.Http.Headers;
+
+namespace BookStoreApp.Blazor.Server.UI.Services.Base
+{
+    public class BaseHttpService
+    {
+        private readonly ILocalStorageService localStorage;
+        private readonly IClient client;
+
+        public BaseHttpService(ILocalStorageService localStorage, IClient client)
+        {
+            this.localStorage = localStorage;
+            this.client = client;
+        }
+
+        protected Response<Guid> ConvertApiExceptions<Guid>(ApiException apiException)
+        {
+            if (apiException.StatusCode == 400)
+            {
+                return new Response<Guid> { Message = "Validation errors have occured.", ValidationErrors = apiException.Response, Success = false };
+            }
+
+            if (apiException.StatusCode == 404)
+            {
+                return new Response<Guid> { Message = "The requested item could not be found.", Success = false };
+            }
+
+            return new Response<Guid>() { Message = "Something went wrong, please try again.", Success = false };
+        }
+
+        protected async Task GetBearerToken()
+        {
+            var token = await localStorage.GetItemAsync<string>("accessToken");
+
+            if(token != null)
+            {
+                client.HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
+            }
+        }
+    }
+}

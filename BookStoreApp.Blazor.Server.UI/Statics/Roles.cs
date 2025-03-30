636e2ebc0fd9034f01efedf69c94fc8202835288
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2025-03-30 17:30:47 +0300

    Add Authorization Restrictions

---
+++ BookStoreApp.Blazor.Server.UI/Statics/Roles.cs
@@ -0,0 +1,8 @@
+﻿namespace BookStoreApp.Blazor.Server.UI.Statics
+{
+    public static class Roles
+    {
+        public const string Administrator = "Administrator";
+        public const string User = "User";
+    }
+}

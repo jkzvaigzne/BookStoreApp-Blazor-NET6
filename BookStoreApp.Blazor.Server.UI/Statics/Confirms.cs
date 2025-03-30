aa890e579b1c0e1a2fde3079d06528964fe36ab5
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2025-03-30 17:34:44 +0300

    Add Static Confirms Class

---
+++ BookStoreApp.Blazor.Server.UI/Statics/Confirms.cs
@@ -0,0 +1,7 @@
+﻿namespace BookStoreApp.Blazor.Server.UI.Statics
+{
+    public static class Confirms
+    {
+        public const string DeleteConfirm = "Are you sure you want to delete this record?";
+    }
+}

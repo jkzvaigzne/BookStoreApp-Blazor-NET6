2b9e0dbdef7ffc2960168c3b4600defabb6c5aad
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2025-03-30 20:07:01 +0300

    Setup Create Book Component w/ File Upload First

---
+++ BookStoreApp.Blazor.Server.UI/Statics/Formats.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStoreApp.Blazor.Server.UI.Statics
+{
+    public static class Formats
+    {
+        public const string JPG = "jpg";
+        public const string PNG = "png";
+        public const string JPGE = "jpeg";
+    }
+}
---
+++ BookStoreApp.Blazor.Server.UI/Statics/Warnings.cs
@@ -0,0 +1,7 @@
+﻿namespace BookStoreApp.Blazor.Server.UI.Statics
+{
+    public static class Warnings
+    {
+        public const string JpgOrPng = "Invalid file format. Please select a valid image file. (*.jpg | *.png)";
+    }
+}
